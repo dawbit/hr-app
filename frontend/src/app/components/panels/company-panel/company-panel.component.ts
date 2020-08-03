5ba59cdc2f0fa197b1619a7cee41ad6363ce1edc
dawbit <dawbitner@gmail.com>
2020-08-03 20:48:43 +0200

    PI-16 Bazowe komponenty, routerLinki

---
+++ frontend/src/app/components/panels/company-panel/company-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-panel',
+  templateUrl: './company-panel.component.html',
+  styleUrls: ['./company-panel.component.scss']
+})
+export class CompanyPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
