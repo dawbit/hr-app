0dcf79c6b13df219ac6e542e8196a96bd2f80041
Nepath <matekow347@student.polsl.pl>
2020-09-16 16:37:18 +0200

    PI-6 dodanie nowej firmy, dodaj od razu założyciela

--- backend/src/main/java/com/hr/app/controllers/CompaniesController.java
+++ backend/src/main/java/com/hr/app/controllers/CompaniesController.java
@@ -1,8 +1,14 @@
 package com.hr.app.controllers;
 
+import com.hr.app.models.CeosModel;
 import com.hr.app.models.CompaniesModel;
+import com.hr.app.models.ResponseTransfer;
+import com.hr.app.repositories.ICeosRepository;
 import com.hr.app.repositories.ICompaniesRepository;
+import com.hr.app.repositories.IUsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +20,33 @@ public class CompaniesController {
     @Autowired
     ICompaniesRepository companiesRepository;
 
+    @Autowired
+    IUsersRepository usersRepository;
+
+    @Autowired
+    ICeosRepository ceosRepository;
+
     @GetMapping("/companies/all")
     public List<CompaniesModel> getAllCompanies() {
         return companiesRepository.findAll();
     }
 
+    @Transactional
     @PostMapping("companies/add")
-    public CompaniesModel addCompanies(@RequestBody CompaniesModel companiesModel) {
+    public ResponseTransfer addCompanies(@RequestBody CompaniesModel companiesModel) {
+        CompaniesModel company = companiesModel;
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
         try {
-            if (getCompanyById(companiesModel.getId()) != null) {
-                return null;
+            if (getCompanyById(company.getId()) != null) {
+                return new ResponseTransfer("nie udalo sie");
             } else {
-                return companiesRepository.save(companiesModel);
+                companiesRepository.save(company);
+                ceosRepository.save(new CeosModel(usersRepository.findByLogin(name), company));
+                return new ResponseTransfer("Udalo sie");
             }
         }
         catch (Exception e) {
-            return null;
+            return new ResponseTransfer("nie udalo sie");
         }
     }
 
