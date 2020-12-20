4ac463989c930dceec2af41e8e2620a72b101042
dawbit <dawbitner@gmail.com>
2020-12-20 22:21:31 +0100

    PI-71 testy backend

---
+++ backend/src/main/java/com/hr/app/tests/SimpleUnitTests.java
@@ -0,0 +1,48 @@
+package com.hr.app.tests;
+
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@EnableWebMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleUnitTests {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
+    @WithMockUser(value = "test123", password = "test")
+    @Test
+    public void checkTypeInUsersList() throws Exception {
+        this.mvc.perform(get("/user/getall")).andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].firstName", Is.isA(String.class)));
+    }
+
+}
