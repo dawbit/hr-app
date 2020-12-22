2fa5026ec26ad9029c50cd875864169071662af9
Nepath <matekow347@student.polsl.pl>
2020-12-22 03:51:35 +0100

    dodanie widoku danych usera wraz z logiką, oraz dodanie pobierania plików

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:mobile/injections/app_module.dart';
 
-void main() => runApp(AppModule());
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: true // optional: set false to disable printing logs to console
+  );
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_){
+    runApp(AppModule());
+  }
+  );
+}
