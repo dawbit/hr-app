d9ffd8a9cbdce5a094b4940fcfb01fe5de903f60
dawbit <dawbitner@gmail.com>
2021-01-23 13:29:52 +0100

    quickfix 4

--- frontend/src/app/components/panels/ceo-panel/delete-from-hr/delete-from-hr.component.ts
+++ frontend/src/app/components/panels/ceo-panel/delete-from-hr/delete-from-hr.component.ts
@@ -76,7 +76,7 @@ export class DeleteFromHrComponent implements OnInit {
   deleteFromHr(userId) {
     this.ceoService.deleteUserFromHr(userId).subscribe(res => {
       this.toasts.showSuccess('ceo.assignSuccess');
-      this.mdbTable.getDataSource();
+      this.hrUsers = this.mdbTable.getDataSource();
     },
       err => {
         this.toasts.showError('ceo.assignError');
